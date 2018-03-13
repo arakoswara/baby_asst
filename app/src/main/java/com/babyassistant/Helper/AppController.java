f28b2de2efc7f7f0660577a4bacb6c91f389d0fe
ara koswara <arakoswara2@gmail.com>
2018-03-13 16:35:46 +0700

    upload add volley, no image

--- app/src/main/java/com/babyassistant/Helper/AppController.java
+++ app/src/main/java/com/babyassistant/Helper/AppController.java
@@ -2,7 +2,16 @@ package com.babyassistant.Helper;
 
 import android.app.Application;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class AppController extends Application {
+
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
+    public static final String TAG = AppController.class.getSimpleName();
 
     @Override
     public void onCreate() {
@@ -12,5 +21,28 @@ public class AppController extends Application {
         FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/Quicksand-Light.ttf");
         FontsOverride.setDefaultFont(this, "SERIF", "fonts/Quicksand-Light.ttf");
         FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/Quicksand-Light.ttf");
+    }
+
+    public static synchronized AppController getInstance()
+    {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue()
+    {
+        if (mRequestQueue == null)
+        {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req)
+    {
+        req.setTag(TAG);
+        req.setRetryPolicy(new DefaultRetryPolicy(
+                60 * 10 * 1000, 0,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        getRequestQueue().add(req);
     }
 }
--- app/src/main/java/com/babyassistant/Helper/Functions.java
+++ app/src/main/java/com/babyassistant/Helper/Functions.java
@@ -1,13 +1,28 @@
 package com.babyassistant.Helper;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 
-/**
- * Created by Irwan on 9/3/18.
- */
+import com.babyassistant.R;
 
 public class Functions {
-    public static Context context;
+    public static Context mContext;
     public static Typeface AppsFont;
+
+    public static void AssignGlobalVariables(Context context) {
+
+    }
+
+    public static void setPreferences(Context context, String key, String value) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(context.getResources().getString(R.string.app_name), context.MODE_PRIVATE).edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public static String getPreferences(Context context, String key)
+    {
+        SharedPreferences prefs = context.getSharedPreferences(context.getResources().getString(R.string.app_name), context.MODE_PRIVATE);
+        return prefs.getString(key,null);
+    }
 }
