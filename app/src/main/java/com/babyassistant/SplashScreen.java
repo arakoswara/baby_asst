ad271766aeb3ce0b51a9aa0f9b19f4f342b7ac92
ara koswara <arakoswara2@gmail.com>
2018-03-22 18:24:03 +0700

    commit add comment

--- app/src/main/java/com/babyassistant/SplashScreen.java
+++ app/src/main/java/com/babyassistant/SplashScreen.java
@@ -19,6 +19,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        // set logo
         logo = (ImageView)findViewById(R.id.logo_splash_screen);
         Picasso.with(this).load(R.drawable.logo).into(logo);
 
