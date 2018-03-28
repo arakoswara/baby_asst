6bb6fd173792b746a7740ee795425a0797cd663e
ara koswara <arakoswara2@gmail.com>
2018-03-28 16:06:38 +0700

    commit add file

---
+++ app/src/main/java/com/babyassistant/Controller/Info/InfoGizi.java
@@ -0,0 +1,15 @@
+package com.babyassistant.Controller.Info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyassistant.R;
+
+public class InfoGizi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_gizi);
+    }
+}
