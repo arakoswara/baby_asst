aed358b30d33fb124d1d3d76b5cfc06eff003992
arakoswara <arakoswara2@gmail.com>
2018-04-19 20:00:51 -0400

    commit update

--- app/src/main/java/com/babyassistant/Controller/Profile/Login.java
+++ app/src/main/java/com/babyassistant/Controller/Profile/Login.java
@@ -1,15 +1,34 @@
 package com.babyassistant.Controller.Profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.babyassistant.R;
 
 public class Login extends AppCompatActivity {
 
+    private LinearLayout ll_login;
+    private EditText et_email, et_password;
+    private Button btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ll_login = (LinearLayout) findViewById(R.id.llToRegister);
+        ll_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/babyassistant/Controller/Profile/Register.java
+++ app/src/main/java/com/babyassistant/Controller/Profile/Register.java
@@ -1,15 +1,37 @@
 package com.babyassistant.Controller.Profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.babyassistant.MainActivity;
 import com.babyassistant.R;
 
 public class Register extends AppCompatActivity {
+
+    private LinearLayout ll_login;
+    private EditText et_email;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ll_login = (LinearLayout) findViewById(R.id.llToLogin);
+        ll_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Register.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
